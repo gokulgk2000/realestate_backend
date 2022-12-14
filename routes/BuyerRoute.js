7a56f64271e5163fc363c4e99fd8cd51fa47fbe0
krishnakumars59809 <116875316+krishnakumars59809@users.noreply.github.com>
2022-12-14 18:27:54 -0500

    check  again  route  4

--- routes/BuyerRoute.js
+++ routes/BuyerRoute.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { hashGenerator } = require("../helpers/Hashing");
-const { hashValidator } = require("../helpers/Hashing");
-const { JWTtokenGenerator } = require("../helpers/token");
+// const { hashGenerator } = require("../helpers/Hashing");
+// const { hashValidator } = require("../helpers/Hashing");
+// const { JWTtokenGenerator } = require("../helpers/token");
 const config = require("../config");
 const BuyerModel = require("../models/BuyerModel");
-const RegPropertyModel = require("../models/RegPropertyModel");
+// const RegPropertyModel = require("../models/RegPropertyModel");
 router.get("/", (req, res) => res.send("Buyer Route"));
 
 
@@ -40,76 +40,76 @@ router.post("/buyerregister", async (req, res) => {
     }
   );
 
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  BuyerModel.findOne({ email: email }, async (err, isUser) => {
-    if (err) {
-      return res.json({
-        msg: "Login failed",
-        error: err,
-      });
-    } else if (!isUser) {
-      return res.json({
-        msg: "This email isn't registered yet",
-      });
-    } else if (!isUser.aflag) {
-      return res.json({
-        msg: "This account has been deactivated",
-      });
-    }
-    // else if(!isUser?.verified){      //For Email Verification
-    //   return res.json({
-    //     msg: "This account hasn't been verified yet",
-    //   });
-    // }
-    else {
-      const result = await hashValidator(password, isUser.password);
-      if (result) {
-        console.log(result, "result");
-        const jwtToken = await JWTtokenGenerator({
-          id: isUser._id,
-          expire: "30d",
-        });
-        const query = {
-          userId: isUser._id,
-          firstname: isUser.firstname,
-          lastname: isUser.lastname,
-          aflag: true,
-          token: "JWT " + jwtToken,
-          profilePic: isUser.profilePic,
-        };
-        res.cookie("jwt", jwtToken, {
-          httpOnly: true,
-          maxAge: 30 * 24 * 60 * 60 * 1000,
-        });
-        console.log("Setting cookie in res");
-        // ActiveSessionModel.create(query, (err, session) => {
-        //   if (err) {
-        //     return res.json({
-        //       msg: "Error Occured!!",
-        //     });
-        //   } else {
-        return res.json({
-          success: true,
-          userID: isUser._id,
-          firstname: isUser.firstname,
-          lastname: isUser.lastname,
-          email: isUser.email,
-          contact: isUser.contact,
-          token: "JWT " + jwtToken,
-          propertyStatus: isUser.propertyStatus,
-          propertyPic: isUser.propertyPic,
-        });
-        //   }
-        // });
-      } else {
-        return res.json({
-          msg: "Password Doesn't match",
-        });
-      }
-    }
-  });
-});
+// router.post("/login", async (req, res) => {
+//   const { email, password } = req.body;
+//   BuyerModel.findOne({ email: email }, async (err, isUser) => {
+//     if (err) {
+//       return res.json({
+//         msg: "Login failed",
+//         error: err,
+//       });
+//     } else if (!isUser) {
+//       return res.json({
+//         msg: "This email isn't registered yet",
+//       });
+//     } else if (!isUser.aflag) {
+//       return res.json({
+//         msg: "This account has been deactivated",
+//       });
+//     }
+//     // else if(!isUser?.verified){      //For Email Verification
+//     //   return res.json({
+//     //     msg: "This account hasn't been verified yet",
+//     //   });
+//     // }
+//     else {
+//       const result = await hashValidator(password, isUser.password);
+//       if (result) {
+//         console.log(result, "result");
+//         const jwtToken = await JWTtokenGenerator({
+//           id: isUser._id,
+//           expire: "30d",
+//         });
+//         const query = {
+//           userId: isUser._id,
+//           firstname: isUser.firstname,
+//           lastname: isUser.lastname,
+//           aflag: true,
+//           token: "JWT " + jwtToken,
+//           profilePic: isUser.profilePic,
+//         };
+//         res.cookie("jwt", jwtToken, {
+//           httpOnly: true,
+//           maxAge: 30 * 24 * 60 * 60 * 1000,
+//         });
+//         console.log("Setting cookie in res");
+//         // ActiveSessionModel.create(query, (err, session) => {
+//         //   if (err) {
+//         //     return res.json({
+//         //       msg: "Error Occured!!",
+//         //     });
+//         //   } else {
+//         return res.json({
+//           success: true,
+//           userID: isUser._id,
+//           firstname: isUser.firstname,
+//           lastname: isUser.lastname,
+//           email: isUser.email,
+//           contact: isUser.contact,
+//           token: "JWT " + jwtToken,
+//           propertyStatus: isUser.propertyStatus,
+//           propertyPic: isUser.propertyPic,
+//         });
+//         //   }
+//         // });
+//       } else {
+//         return res.json({
+//           msg: "Password Doesn't match",
+//         });
+//       }
+//     }
+//   });
+// });
 
 router.get("/allBuyerList", async (req, res) => {
   BuyerModel.find({})
