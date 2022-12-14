1125d09ce5964f3c1e526c1bfb85aa0b8f593dfe
krishnakumars59809 <116875316+krishnakumars59809@users.noreply.github.com>
2022-12-14 18:48:09 -0500

    mongodb cloud connect

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 const config={
-    MONGO_URL: "mongodb://0.0.0.0:27017/CovaiRealEstate",    JWT_SECRET: "dev",
+    MONGO_URL: "mongodb+srv://krishnakumar:krishna123456.@cluster0.c05tjkl.mongodb.net/?retryWrites=true&w=majority/test",    
+    JWT_SECRET: "dev",
     PORT: 8000,
-    // MAIL_SERVICE: "gmail",
-
     SENDER_MAIL: "dspadmanaban2000@gmail.com",
     MAIL_PASSWORD: "Re1234567",
     RESET_PASSWORD: "dev123",
