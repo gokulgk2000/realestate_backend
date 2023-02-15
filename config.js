7cd83169922d0c3b5bd663555d625af4bfc94fa5
Vidhya <vidanu11@gmail.com>
2023-02-15 12:16:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const config={
+const config = {
+  MONGO_URL:
+    "mongodb://realestate:APVp46VdnzPOpYEWg6WH4GvkkTiZ6ErDAtqonx2Apb1R0MZN60QIUqurbgamtMKRHHIcdEFkIDSfACDbTsMkAQ==@realestate.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@realestate@",
 
-    // MONGO_URL: "mongodb://localhost:27017/CovaiRealEstate",    
-
-    MONGO_URL: "mongodb+srv://krishnakumar:krishna123456.@cluster0.c05tjkl.mongodb.net/test?retryWrites=true&w=majority",    
-    // MONGO_URL: "mongodb://0.0.0.0:27017/CovaiRealEstate",
-    JWT_SECRET: "dev",
-    PORT: 8000,
-    SENDER_MAIL: "dspadmanaban2000@gmail.com",
-    MAIL_PASSWORD: "Re1234567",
-    RESET_PASSWORD: "dev123",
-}
+  //MONGO_URL: "mongodb+srv://krishnakumar:krishna123456.@cluster0.c05tjkl.mongodb.net/test?retryWrites=true&w=majority",
+  // MONGO_URL: "mongodb://0.0.0.0:27017/CovaiRealEstate",
+  JWT_SECRET: "dev",
+  PORT: 8000,
+  SENDER_MAIL: "dspadmanaban2000@gmail.com",
+  MAIL_PASSWORD: "Re1234567",
+  RESET_PASSWORD: "dev123",
+};
 module.exports = config;
